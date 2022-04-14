34084e84b5f54bc9a210e870bc3d20aa08a9563b
tomonari <zcsxcacg@i.softbank.jp>
2022-04-14 22:30:38 +0900

    タイムゾーンの問題を修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -3,33 +3,36 @@ const generateYear = (argv, today) => {
   else return today.getFullYear()
 }
 const generateMonth = (argv, today) => {
-  if (Object.prototype.hasOwnProperty.call(argv, 'm')) return argv.m
+  if (Object.prototype.hasOwnProperty.call(argv, 'm')) return argv.m - 1
   else return today.getMonth()
 }
 const generateLastday = (day, month) => {
-  day.setMonth(month)
+  day.setMonth(month + 1)
   day.setDate(0)
   return day.getDate()
+}
+const generateCalendar = (year, month, lastday, wday) => {
+  process.stdout.write('      ' + (month + 1) + '月 ' + year + '\n')
+  process.stdout.write(' 日 月 火 水 木 金 土' + '\n')
+  for (let number = 1; number <= wday; number++) {
+    process.stdout.write('   ')
+  }
+  for (let days = 1; days <= lastday; days++) {
+    if (days < 10) {
+      process.stdout.write(' ')
+    }
+    process.stdout.write(String(days) + ' ')
+    if ((days + wday) % 7 === 0) {
+      process.stdout.write('\n')
+    }
+  }
 }
 const argv = require('minimist')(process.argv.slice(2))
 const today = new Date()
 const year = generateYear(argv, today)
 const month = generateMonth(argv, today)
-const firstday = new Date(parseInt(year), parseInt(month), 1)
-const lastday = generateLastday(firstday, month)
+const timeDifference = 9
+const firstday = new Date(year, month, 1, timeDifference)
 const wday = firstday.getDay()
-console.log(firstday)
-process.stdout.write('      ' + (month + 1) + '月 ' + year + '\n')
-process.stdout.write(' 日 月 火 水 木 金 土' + '\n')
-for (let number = 0; number < (wday + 1) % 7; number++) {
-  process.stdout.write('   ')
-}
-for (let days = 1; days <= lastday; days++) {
-  if (days < 10) {
-    process.stdout.write(' ')
-  }
-  process.stdout.write(String(days) + ' ')
-  if ((days + wday) % 7 === 6) {
-    process.stdout.write('\n')
-  }
-}
+const lastday = generateLastday(firstday, month)
+generateCalendar(year, month, lastday, wday)
