d2f5188ef01e1d78f3b644a21cdd3aad10d718fb
tomonari <zcsxcacg@i.softbank.jp>
2022-04-17 23:00:42 +0900

    スペース、演算子、文字列の修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
-for( let number = 1; number <= 20; number++ ) {
-  if( number % 15 == 0 ) {
-    console.log( 'fizzbuzz' );
-  } else if( number % 3 == 0 ) {
-    console.log( 'fizz' );
-  } else if( number % 5 == 0 ) {
-    console.log( 'buzz' );
+for (let number = 1; number <= 20; number++) {
+  if (number % 15 === 0) {
+    console.log('FizzBuzz');
+  } else if (number % 3 === 0) {
+    console.log('Fizz');
+  } else if (number % 5 === 0) {
+    console.log('Buzz');
   } else {
-    console.log( number );
+    console.log(number);
   }
 }
